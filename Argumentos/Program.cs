ad7db1109aa9619b06d487a45a9c2fcabf01e497
LizardonPT <joaofilipegoncalves2000@gmail.com>
2022-02-23 20:17:37 +0000

    Args exercise

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Argumentos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+            
+        }
+    }
+}
