99e4be0f39e6e5d1e703191334cc7c37bd2f7a1a
LizardonPT <joaofilipegoncalves2000@gmail.com>
2022-02-24 12:42:36 +0000

    Strings exercise 5

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -8,8 +8,17 @@ namespace VariasStrings
         {
             string simple = "This is a normal string";
             string verbatim = @"C:\Users\Joao\Documents";
+            string ola = "Olá";
+            string world = "World";
+            int valor = 1;
+            string s = String.Format("O número {0} é equivalente à letra {1}", 5, "B");
+
             Console.WriteLine(simple);
             Console.WriteLine(verbatim);
+            Console.WriteLine(ola + " " + world);
+            Console.WriteLine("This " + world);
+            Console.WriteLine($"O valor é {valor}");
+            Console.WriteLine(s);
         }
     }
 }
