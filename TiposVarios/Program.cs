8aa59e66d79e42b063c480d0e17bb89ca58a04a6
LizardonPT <joaofilipegoncalves2000@gmail.com>
2022-02-24 11:39:13 +0000

    Ex3.5

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -6,18 +6,19 @@ namespace TiposVarios
     internal class Program
     {
         
-        
         static void Main(string[] args)
         {
             int number = 3;
             float pi = 3.14f;
             double piplus = 2.14159;
             decimal z = 9.9999m;
-            
+            char emoji = '\u00A5';
+
             Console.WriteLine(number.ToString());
             Console.WriteLine(pi.ToString());
             Console.WriteLine(piplus.ToString());
             Console.WriteLine(z.ToString());
+            Console.WriteLine(emoji);
         }
     }
 }
