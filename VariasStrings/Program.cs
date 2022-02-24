e051b9c5ed9c2022636e04334b9752f2ae743a31
LizardonPT <joaofilipegoncalves2000@gmail.com>
2022-02-24 12:25:11 +0000

    Strings exercise

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string simple = "This is a normal string";
+            string verbatim = @"C:\Users\Joao\Documents";
+            Console.WriteLine(simple);
+            Console.WriteLine(verbatim);
+        }
+    }
+}
