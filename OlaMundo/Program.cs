0f8a56bfaf28edf6ba0bf4b1b7c88a38c7b8acf8
LizardonPT <joaofilipegoncalves2000@gmail.com>
2022-02-24 11:31:50 +0000

    Ex3.2

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-
+/// <summary>
+/// This is a summary i guess
+/// </summary>
 namespace OlaMundo // Note: actual namespace depends on the project name.
 {
     internal class Program
---
+++ TiposVarios/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace TiposVarios
+{
+    internal class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            int number = 3;
+            float pi = 3.14f;
+            double piplus = 2.14159;
+            decimal z = 9.9999m;
+            
+            Console.WriteLine(number.ToString());
+            Console.WriteLine(pi.ToString());
+            Console.WriteLine(piplus.ToString());
+            Console.WriteLine(z.ToString());
+        }
+    }
+}
